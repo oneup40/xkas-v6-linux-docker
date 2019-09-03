70a57afcc9c5d82ce8ec3f7dad078f92d25801f4
oneup40 <oneup40@gmail.com>
2019-09-03 12:32:26 -0400

    compare stdout and stderr too

--- test/run.sh
+++ test/run.sh
@@ -19,9 +19,15 @@ cp /test/base.sfc $orig_sfc
 cp /test/xkas-linux $gitdir/z3randomizer/
 cp /test/xkas-orig.exe $gitdir/z3randomizer/
 
+linux_stdout="$(mktemp)"
+linux_stderr="$(mktemp)"
+
+orig_stdout="$(mktemp)"
+orig_stderr="$(mktemp)"
+
 pushd $gitdir/z3randomizer >/dev/null
-./xkas-linux LTTP_RND_GeneralBugfixes.asm $linux_sfc
-wine ./xkas-orig.exe LTTP_RND_GeneralBugfixes.asm $orig_sfc
+./xkas-linux LTTP_RND_GeneralBugfixes.asm $linux_sfc >linux_stdout 2>linux_stderr
+wine ./xkas-orig.exe LTTP_RND_GeneralBugfixes.asm $orig_sfc >orig_stdout 2>orig_stderr
 popd >/dev/null
 
 linux_md5sum="$(mktemp)"
@@ -35,12 +41,34 @@ exitcode=0
 if diff $linux_md5sum $orig_md5sum ; then
     true
 else
-    echo "mismatch on commit $1"
+    echo "ROM mismatch on commit $1"
+    exitcode=1
+fi
+
+md5sum - <$linux_stdout > $linux_md5sum
+md5sum - <$orig_stdout > $orig_md5sum
+
+if diff $linux_md5sum $orig_md5sum ; then
+    true
+else
+    echo "stdout mismatch on commit $1"
+    exitcode=1
+fi
+
+md5sum - <$linux_stderr > $linux_md5sum
+md5sum - <$orig_stderr > $orig_md5sum
+
+if diff $linux_md5sum $orig_md5sum ; then
+    true
+else
+    echo "stderr mismatch on commit $1"
     exitcode=1
 fi
 
 rm $linux_sfc $orig_sfc
-rm $linux_md5sum $orig2_md5sum
+rm $linux_stdout $orig_stdout
+rm $linux_stderr $orig_stderr
+rm $linux_md5sum $orig_md5sum
 rm -rf $gitdir
 
 exit $exitcode
